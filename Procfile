b1bbb5f02e198767c51eabc26be5d503c6debc1e
abir <abir4u2011@gmail.com>
2020-02-29 15:19:42 +0600

    procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cWork_inventory.wsgi --log-file-
--- cWork_inventory/settings.py
+++ cWork_inventory/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nwtbn3*jrvkjy9n(heomb%g5vg$yh(l1rof8rzk$9)!)b#c(mm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
